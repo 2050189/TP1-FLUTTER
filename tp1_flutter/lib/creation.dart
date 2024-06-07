b8fed39020f473e31892584c582a42612b67cce1
sarah <2050189@cegepmontpetit.ca>
2024-06-07 15:36:00 -0400

    PROGRES Ajout des autres pages
    Elles ne sont pas complètes encore.

---
+++ tp1_flutter/lib/creation.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Creation extends StatefulWidget {
+  const Creation({super.key});
+
+  @override
+  State<Creation> createState() => _CreationState();
+}
+
+class _CreationState extends State<Creation> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Column(
+
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
